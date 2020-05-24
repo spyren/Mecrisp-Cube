cfd4e2ab94362380d15fcce1571b901d35306f2b
Peter Schmid <peter@spyr.ch>
2020-05-24 22:22:44 +0200

    Christmas tree on hard fault.

--- Core/Src/stm32wbxx_it.c
+++ Core/Src/stm32wbxx_it.c
@@ -92,7 +92,10 @@ void NMI_Handler(void)
 void HardFault_Handler(void)
 {
   /* USER CODE BEGIN HardFault_IRQn 0 */
-
+	// christmas tree
+	HAL_GPIO_WritePin(LD3_GPIO_Port, LD1_Pin, GPIO_PIN_SET);
+	HAL_GPIO_WritePin(LD3_GPIO_Port, LD2_Pin, GPIO_PIN_SET);
+	HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_SET);
   /* USER CODE END HardFault_IRQn 0 */
   while (1)
   {
