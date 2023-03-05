eee64ceb8b963e175da2b9f8fd19b27cc86d67f1
Peter Schmid <peter@spyr.ch>
2023-03-05 16:07:02 +0100

    Fix assert conditional problem

--- peripherals/myassert.h
+++ peripherals/myassert.h
@@ -101,7 +101,7 @@ __attribute__( ( always_inline ) ) static inline uint32_t __get_PC(void)
  *  @return
  *      None
  */
-#ifdef CFG_ASSERT_ON
+#if CFG_ASSERT_ON == 1
 #define ASSERT_nonfatal(cond, id, param)                \
   if (!(cond)) {                                        \
 	RTC_Backup.assert = RTC_MAGIC_COOKIE;               \
